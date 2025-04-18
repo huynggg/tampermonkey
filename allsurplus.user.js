933ac00b439aad824038de51c224e497bec7ed62
huynggg <huyanguyen3695@gmail.com>
2025-04-18 18:17:17 -0500

    testing

--- allsurplus.user.js
+++ allsurplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         allsurplus_helpers
 // @namespace    http://tampermonkey.net/
-// @version      1.7
+// @version      1.7.1
 // @description  Helpers for using allsurplus.com
 // @author       Huy Nguyen
 // @match        https://www.allsurplus.com/*
